58e635dd3978da45f04c2b3d48dc48e445bdb954
s145469 <luud-@live.nl>
2018-01-30 17:49:27 +0100

    Fix diameter by using largest connected component

--- analysis/diameter.py
+++ analysis/diameter.py
@@ -1,7 +1,9 @@
 import graph_tool
 from graph_tool.topology import pseudo_diameter
+from analysis.lcc import graph_lcc
 
 
 # Calculates the diameter of a graph
 def graph_diameter(graph):
-	return int(pseudo_diameter(graph)[0])
+	largest_connected_component = graph_lcc(graph)
+	return int(pseudo_diameter(largest_connected_component)[0])
